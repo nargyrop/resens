011e4441ea382687ff08cd827461c4d910f9e730
nickargirop <nickargirop@gmail.com>
2020-02-28 20:54:54 +0000

    Copy from gitlab

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyscisys',
+      version='0.2',
+      description='Scisys EO python package',
+      url='http://spcbs-svrep0/argyropoulos_n/pyscisys.git',
+      author='Nikos Argyropoulos',
+      author_email='nikos.argyropoulos@scisys.co.uk',
+      license='',
+      packages=['pyscisys'],
+      install_requires=['gdal', 'numpy', 'opencv-python'],
+      python_requires='>=3.6.5',
+      zip_safe=False)
