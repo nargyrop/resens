dcc22b88ce316b116661f83202e1c511f75b6147
nargyrop <58083277+nargyrop@users.noreply.github.com>
2020-09-07 13:25:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pyna',
       version='0.2',
       description='Nikos Argyropoulos EO python package',
-      url='https://www.nargyrop.me',
+      url='https://www.nargyrop.com',
       author='Nikos Argyropoulos',
       author_email='n.argiropeo@gmail.com',
       license='',
