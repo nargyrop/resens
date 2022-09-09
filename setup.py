89b3d848f28930ffc71da843a07ba2f82bc9cd1f
Nikos Argyropoulos <n.argyropoulos@oilx.co>
2022-09-09 22:45:15 +0300

    Some more refactoring and improvements

--- setup.py
+++ setup.py
@@ -2,31 +2,12 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-# Check for dependencies
-install_requires = []
-try:
-    import numpy
-except ImportError:
-    install_requires.append("numpy")
-try:
-    try:
-        import gdal
-    except ImportError:
-        from osgeo import gdal
-except ImportError:
-    install_requires.append("GDAL>=3.*")
-try:
-    import cv2
-except ImportError:
-    install_requires.append("opencv-python")
-
-
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="resens",
-    version="0.4.2",
+    version="0.4.3",
     description="Raster Processing package for Remote Sensing and Earth Observation",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -34,9 +15,13 @@ setup(
     author="Nikos Argyropoulos",
     author_email="n.argiropgeo@gmail.com",
     license="MIT",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    python_requires=">=3.7",
+    packages=find_packages("resens"),
+    package_dir={"resens": "resens"},
+    python_requires=">=3.9",
     zip_safe=False,
-    install_requires=install_requires
+    install_requires=[
+        "numpy",
+        "GDAL>=3.*",
+        "opencv-python"
+    ]
 )
