52d90ae45e99c55ce67d29b643b391b324270761
Nikos Argyropoulos <n.argyropoulos@oilx.co>
2022-09-10 16:37:58 +0300

    Bug fix in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="resens",
-    version="0.4.3",
+    version="0.4.3.1",
     description="Raster Processing package for Remote Sensing and Earth Observation",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     author="Nikos Argyropoulos",
     author_email="n.argiropgeo@gmail.com",
     license="MIT",
-    packages=find_packages("resens"),
+    packages=["resens"],
     package_dir={"resens": "resens"},
     python_requires=">=3.9",
     zip_safe=False,
