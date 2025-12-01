b8b88d976914189aa23dc0401ab4abfb513f43cf
Nikos Argyropoulos <n.argyropoulos@oilx.co>
2025-12-01 11:19:23 +0000

    setup.py fix for github actions

--- setup.py
+++ setup.py
@@ -3,10 +3,22 @@ from typing import List
 
 from setuptools import setup
 
-from resens.__version__ import __version__
-
 BASE_DIR = Path(__file__).parent
 long_description = (BASE_DIR / "README.md").read_text()
+
+
+def read_version() -> str:
+    """Function to read the package version
+
+    :return: Package version
+    :rtype: str
+    """
+
+    version_file = BASE_DIR / "resens" / "__version__.py"
+    context = {}
+    exec(version_file.read_text(), context)
+
+    return context["__version__"]
 
 
 def get_required() -> List[str]:
@@ -29,11 +41,10 @@ def get_required() -> List[str]:
 
 setup(
     name="resens",
-    version=__version__,
+    version=read_version(),
     description="Raster Processing package for Remote Sensing and Earth Observation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://www.nargyrop.com",
     author="Nikos Argyropoulos",
     author_email="n.argiropgeo@gmail.com",
     license="MIT",
